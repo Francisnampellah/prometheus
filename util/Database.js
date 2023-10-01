5452cd77c764c1cf2e68f90e7a3e9ce1286fa943
FrancisNampellah <bnampellah1@gmail.com>
2023-10-01 18:40:14 +0300

    todays

--- util/Database.js
+++ util/Database.js
@@ -1,10 +1,8 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize(
-  "novus",
-  "novusPrime",
-  "Juan2010",
-  { dialect: "mysql", host: "localhost" }
-);
+const sequelize = new Sequelize("novus", "novusPrime", "Juan2010", {
+  dialect: "mysql",
+  host: "34.135.151.211",
+});
 
 module.exports = sequelize;
